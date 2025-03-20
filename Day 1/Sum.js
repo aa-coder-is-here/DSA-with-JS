e0944cce7c651db650b6c473d4c936285bb05f54
Ali Arif <aliarif46807@gmail.com>
2025-03-20 14:34:09 +0500

    The sum and concatenation is done babes :)))))

---
+++ Day 1/Sum.js
@@ -0,0 +1,23 @@
+// Sum of two numbers (I know this basic but we have to do it 😒)
+
+let a = 10;
+let b = 20;
+
+console.log(a+b);
+
+// Now see what is the difference between the number and string let's see 10 is a number and if "10" now this is not a number this is a string
+
+//Now time to talk about concatenation yehhhhhhh....
+
+let c = 10;
+let d = "10";
+
+// I you don't about this you say it gonna be 20 but one is string and the other is a number. So, JS gonna do concatenation there.
+
+console.log(c+d);
+
+// So it just combine the string and the number "10" + 10 is 1010. Now what's the type of this
+
+console.log(typeof(c+d)); // It's a string
+
+// It's means if the "+" sign is used it's not always addition it can be concatenated tooadd
---
+++ Day 1/index.js
@@ -0,0 +1,21 @@
+// Difference Between let and var in JS
+
+// This var
+
+var b = 10;
+console.log(b);
+
+a = 12;
+console.log(a);
+var a;
+
+// The Var is working with both which means you can declare the variable before you use and after too :);
+
+let c = 10;
+console.log(c);
+
+d = 12;
+console.log(d);
+let d;
+
+// But but this is not working with let :(
